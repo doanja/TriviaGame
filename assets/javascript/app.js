27915f32efe05edfcb2ddb7e9d40856aba66e3ab
John Doan <doanja@plu.edu>
2019-09-19 18:22:39 -0700

    added a total of 23 trivia questions to trivia.json, changed app.js to read from the json into the dictionary and used a counter to set the max number of question per match to MAX_QUESTIONS_ASKED

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
-let dict = [];
-let copyOfDict = [];
+let dict;
 let correctAnswers, incorrectAnswers, unanswered, qstnCounter; // counters for player score
 let questionTime, questionInterval, breakTime, breakTimeout; // time variables
 const MAX_QUESTION_TIME = 30; // max time for the questionTime
 const MAX_BREAK_TIME = 5; // max time for the breakTime
+const MAX_QUESTIONS_ASKED = 10; // maximum number of question asked
 
 /**
  * function to create and render the question
@@ -11,15 +11,15 @@ const MAX_BREAK_TIME = 5; // max time for the breakTime
  * @param {object} qstn the question to append to the html
  */
 const renderQuestion = (parentElement, qstn) => {
-  // create the elements
-  const time = $('<h5>', { id: 'time' }).text('Time Remaining: 30 Seconds');
-  const question = $('<h3>', { id: 'question' }).text(qstn);
-  const msg = $('<h4>', { id: 'msg' });
-  const subMsg = $('<h4>', { id: 'subMsg' });
-  const row = $('<div>', { class: 'row' });
+	// create the elements
+	const time = $('<h5>', { id: 'time' }).text('Time Remaining: 30 Seconds');
+	const question = $('<h3>', { id: 'question' }).text(qstn);
+	const msg = $('<h4>', { id: 'msg' });
+	const subMsg = $('<h4>', { id: 'subMsg' });
+	const row = $('<div>', { class: 'row' });
 
-  // append the elements to the html
-  parentElement.append(time, question, msg, subMsg, row);
+	// append the elements to the html
+	parentElement.append(time, question, msg, subMsg, row);
 };
 
 /**
@@ -29,35 +29,35 @@ const renderQuestion = (parentElement, qstn) => {
  * @param {string} answr the answer for a question
  */
 const renderAnswers = (parentElement, num, answr) => {
-  //create the elements
-  const col = $('<col>', { class: 'col-12 mt-3' });
-  const answers = $('<button>', {
-    class: 'btn btn-outline-light',
-    id: 'answers-' + num
-  }).text(answr);
+	//create the elements
+	const col = $('<col>', { class: 'col-12 mt-3' });
+	const answers = $('<button>', {
+		class: 'btn btn-outline-light',
+		id: 'answers-' + num
+	}).text(answr);
 
-  // append the elements to the html
-  col.append(answers);
-  parentElement.append(col);
+	// append the elements to the html
+	col.append(answers);
+	parentElement.append(col);
 };
 
 /**
  * function to create and render the end game stats
  * @param {object} parentElement the element to append the question to
  */
-const renderEndGameStats = parentElement => {
-  // create elements
-  const correctTxt = $('<h4>').text('Correct Answers: ' + correctAnswers);
-  const incorrectTxt = $('<h4>').text('Incorrect Answers: ' + incorrectAnswers);
-  const unansweredTxt = $('<h4>').text('Unaswered: ' + unanswered);
-  const hr = $('<hr>');
-  const button = $('<button>', {
-    id: 'start-over-btn',
-    class: 'btn btn-outline-light'
-  }).text('Start Over?');
+const renderEndGameStats = (parentElement) => {
+	// create elements
+	const correctTxt = $('<h4>').text('Correct Answers: ' + correctAnswers);
+	const incorrectTxt = $('<h4>').text('Incorrect Answers: ' + incorrectAnswers);
+	const unansweredTxt = $('<h4>').text('Unaswered: ' + unanswered);
+	const hr = $('<hr>');
+	const button = $('<button>', {
+		id: 'start-over-btn',
+		class: 'btn btn-outline-light'
+	}).text('Start Over?');
 
-  // append the elements to the html
-  parentElement.append(correctTxt, incorrectTxt, unansweredTxt, hr, button);
+	// append the elements to the html
+	parentElement.append(correctTxt, incorrectTxt, unansweredTxt, hr, button);
 };
 
 /**
@@ -65,49 +65,51 @@ const renderEndGameStats = parentElement => {
  * @param {object} problem a random object containing an answer with an array of answers
  */
 const loadQstn = (problem = dict[Math.floor(Math.random() * dict.length)]) => {
-  // timer countdown for each question
-  questionInterval = setInterval(timeForCurrentQuestion, 1000);
+	// reference to the correct answer stored in index 0 of problem.answers
+	const theCorrectAnswer = problem.answers[0];
 
-  // clear previous question (if any)
-  $('#answersWrap').empty();
+	// create a mutable copy of problem.answers
+	const copyOfAnswers = [...problem.answers];
 
-  // renders the question
-  renderQuestion($('#answersWrap'), problem.question);
+	// timer countdown for each question
+	questionInterval = setInterval(timeForCurrentQuestion, 1000);
 
-  // reference to the correct answer stored in index 0 of problem.answers
-  const theCorrectAnswer = problem.answers[0];
+	// clear previous question (if any)
+	$('#answersWrap').empty();
 
-  // create a mutable copy of problem.answers
-  const copyOfAnswers = [...problem.answers];
+	// renders the question
+	renderQuestion($('#answersWrap'), problem.question);
 
-  // render the answers
-  for (let i = 0; i < 4; i++) {
-    // grab a random answer
-    const randomAnswer =
-      copyOfAnswers[Math.floor(Math.random() * copyOfAnswers.length)];
+	// render the answers
+	for (let i = 0; i < 4; i++) {
+		// grab a random answer
+		const randomAnswer =
+			copyOfAnswers[Math.floor(Math.random() * copyOfAnswers.length)];
 
-    // render a button with the random answer as text
-    renderAnswers($('.row'), i, randomAnswer);
+		// render a button with the random answer as text
+		renderAnswers($('.row'), i, randomAnswer);
 
-    // remove the answer from the array
-    copyOfAnswers.splice(copyOfAnswers.indexOf(randomAnswer), 1); // remove the random answer
+		// remove the answer from the array
+		copyOfAnswers.splice(copyOfAnswers.indexOf(randomAnswer), 1); // remove the random answer
 
-    // attach a click listener to the button and check the answer
-    $('#answers-' + i).click(() => {
-      // if the answer is correct
-      if (randomAnswer === theCorrectAnswer) {
-        answerClicked(0, theCorrectAnswer);
-      }
+		// attach a click listener to the button and check the answer
+		$('#answers-' + i).click(() => {
+			// if the answer is correct
+			if (randomAnswer === theCorrectAnswer) {
+				answerClicked(0, theCorrectAnswer);
+			}
 
-      // if the answer is incorrect
-      else if (randomAnswer !== theCorrectAnswer) {
-        answerClicked(1, theCorrectAnswer);
-      }
-    });
-  }
+			// if the answer is incorrect
+			else if (randomAnswer !== theCorrectAnswer) {
+				answerClicked(1, theCorrectAnswer);
+			}
+		});
+	}
 
-  // remove
-  dict.splice(dict.indexOf(problem), 1);
+	// remove
+	dict.splice(dict.indexOf(problem), 1);
+
+	qstnCounter++;
 };
 
 /**
@@ -115,33 +117,33 @@ const loadQstn = (problem = dict[Math.floor(Math.random() * dict.length)]) => {
  * when the questionTime reaches 0, function also handles incrementing noAnswers counter
  */
 const timeForCurrentQuestion = () => {
-  questionTime--; // decrement time
-  $('#time').text('Time Remaining: ' + questionTime + ' Seconds'); // render the countdown
+	questionTime--; // decrement time
+	$('#time').text('Time Remaining: ' + questionTime + ' Seconds'); // render the countdown
 
-  // when the questionTime reaches 0
-  if (questionTime === 0) {
-    unanswered++;
-    $('#msg')
-      .text('Time has ran out...')
-      .attr('class', 'text-danger');
-    nextQuestionCountdown();
-  }
+	// when the questionTime reaches 0
+	if (questionTime === 0) {
+		unanswered++;
+		$('#msg')
+			.text('Time has ran out...')
+			.attr('class', 'text-danger');
+		nextQuestionCountdown();
+	}
 };
 
 /**
  * function to clear the questionTime countdown, reset it, and start the breakTime countdown
  */
 const nextQuestionCountdown = () => {
-  $('.row').empty();
-  clearInterval(questionInterval); // stop the countdown
-  questionTime = MAX_QUESTION_TIME; // reset the time
-  if (dict.length === 0) {
-    endScreen();
-  } else {
-    breakTimeout = setTimeout(() => {
-      loadQstn(); // start the countdown for the next question
-    }, 5000);
-  }
+	$('.row').empty();
+	clearInterval(questionInterval); // stop the countdown
+	questionTime = MAX_QUESTION_TIME; // reset the time
+	if (qstnCounter === MAX_QUESTIONS_ASKED) {
+		endScreen();
+	} else {
+		breakTimeout = setTimeout(() => {
+			loadQstn(); // start the countdown for the next question
+		}, 5000);
+	}
 };
 
 /**
@@ -150,34 +152,34 @@ const nextQuestionCountdown = () => {
  * @param {string} correctAnswer the correct answer
  */
 const answerClicked = (isCorrect, correctAnswer) => {
-  // start breakTime countdown
-  nextQuestionCountdown();
+	// start breakTime countdown
+	nextQuestionCountdown();
 
-  // disable button clicks
-  $('.row').empty();
+	// disable button clicks
+	$('.row').empty();
 
-  // if the answer is correct
-  if (isCorrect === 0) {
-    correctAnswers++;
+	// if the answer is correct
+	if (isCorrect === 0) {
+		correctAnswers++;
 
-    // update the html to show the player got the question right
-    $('#msg')
-      .text('Correct')
-      .attr('class', 'text-success');
-  }
+		// update the html to show the player got the question right
+		$('#msg')
+			.text('Correct')
+			.attr('class', 'text-success');
+	}
 
-  // if the answer was incorrect
-  else {
-    incorrectAnswers++;
+	// if the answer was incorrect
+	else {
+		incorrectAnswers++;
 
-    // update html to show player got the question wrong and display the correct answer
-    $('#msg')
-      .text('Incorrect')
-      .attr('class', 'text-danger');
-    $('#subMsg')
-      .attr('class', 'text-danger')
-      .text('The correct answer was: ' + correctAnswer);
-  }
+		// update html to show player got the question wrong and display the correct answer
+		$('#msg')
+			.text('Incorrect')
+			.attr('class', 'text-danger');
+		$('#subMsg')
+			.attr('class', 'text-danger')
+			.text('The correct answer was: ' + correctAnswer);
+	}
 };
 
 /**
@@ -185,51 +187,28 @@ const answerClicked = (isCorrect, correctAnswer) => {
  * rendering the end game stats screen
  */
 const endScreen = () => {
-  // clear the #answerWrap
-  $('#answersWrap').empty();
+	// clear the #answerWrap
+	$('#answersWrap').empty();
 
-  // clear the question timer
-  clearInterval(questionInterval);
+	// clear the question timer
+	clearInterval(questionInterval);
 
-  // render end game stats
-  renderEndGameStats($('#answersWrap'));
+	// render end game stats
+	renderEndGameStats($('#answersWrap'));
 
-  // when the player clicks the start over button, restart the game
-  $('#start-over-btn').click(initGlobals);
+	// when the player clicks the start over button, restart the game
+	$('#start-over-btn').click(initGlobals);
 };
 
 /**
  * function to add questions to the array and create a immutable duplciate of dict
  */
-const loadDictionary = () => {
-  dict.push(
-    {
-      question: 'What is the diameter of Earth?',
-      answers: ['8,000 miles', '9,300 miles', '7,800 miles', '10,680 miles']
-    },
-    {
-      question: 'Great Whites and Hammerheads are what type of animals?',
-      answers: ['Sharks', 'Dolphins', 'Whales', 'Fish']
-    },
-    {
-      question:
-        'Which famous nurse was known as “The Lady Of The Lamp” during the crimean war?',
-      answers: [
-        'Florence Nightingale',
-        'Marie Curie',
-        'Jane Austen',
-        'Eleanor of Aquitaine'
-      ]
-    },
-    {
-      question: 'If you boil water you get?',
-      answers: ['Steam', 'Water', 'Air', 'Oxygen']
-    },
-    {
-      question: 'Where did the Olympic Games originate?',
-      answers: ['Greece', 'Brazil', 'China', 'Canada']
-    }
-  );
+const loadDictionary = (url) => {
+	$.getJSON(url, (data) => {
+		data.forEach((obj) => {
+			dict.push(obj);
+		});
+	});
 };
 
 /**
@@ -237,21 +216,24 @@ const loadDictionary = () => {
  * and initialize global variables
  */
 const initGlobals = () => {
-  // clear the game
-  $('#answersWrap').empty();
-  $('#answersWrap').hide();
+	// clear the game
+	$('#answersWrap').empty();
+	$('#answersWrap').hide();
 
-  // show the start screen
-  $('#startWrap').show();
+	// show the start screen
+	$('#startWrap').show();
 
-  // initialize variables / reset variables
-  correctAnswers = 0;
-  incorrectAnswers = 0;
-  unanswered = 0;
-  questionTime = MAX_QUESTION_TIME;
-  breakTime = MAX_BREAK_TIME;
+	// initialize variables / reset variables
+	correctAnswers = 0;
+	incorrectAnswers = 0;
+	unanswered = 0;
+	qstnCounter = 0;
+	questionTime = MAX_QUESTION_TIME;
+	breakTime = MAX_BREAK_TIME;
+	dict = [];
 
-  loadDictionary(); // fill the dictionary with triva questions
+	// fill the dictionary with triva questions
+	loadDictionary('/assets/files/trivia.json');
 };
 
 /**
@@ -259,14 +241,12 @@ const initGlobals = () => {
  * loads dictionary and loads a question
  */
 const loadGame = () => {
-  $('#startWrap').hide(); // hide the start screen
-
-  $('#answersWrap').show(); // show the game screen
-
-  loadQstn(); // load the first question
+	$('#startWrap').hide(); // hide the start screen
+	$('#answersWrap').show(); // show the game screen
+	loadQstn(); // load the first question
 };
 
 window.onload = () => {
-  initGlobals();
-  $('#play-button').click(loadGame);
+	initGlobals();
+	$('#play-button').click(loadGame);
 };
